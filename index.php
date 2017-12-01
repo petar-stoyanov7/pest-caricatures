ef26bce6c3f7da2931ccc2f69b082bf1404c961a
Petar Stoyanov <petar.stoyanov@gmail.com>
2017-12-01 15:39:22 +0200

    Start of the project

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Language" content="bg">
+	<link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+	<title>Карикатури pest</title>
+</head>
+<body>
+	<h1 style="position: center;">WORK IN PROGRESS</h1>
+</body>
+</html>
